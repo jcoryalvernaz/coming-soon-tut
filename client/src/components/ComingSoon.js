ad4def9a93ad127621700af97bac97bfbe04c758
Jason Cory Alvernaz <alvernazjason@gmail.com>
2020-01-16 20:25:05 -0800

    chore: changed countdown date

--- client/src/components/ComingSoon.js
+++ client/src/components/ComingSoon.js
@@ -20,7 +20,7 @@ import '../styles/ComingSoon.css';
 class ComingSoon extends Component {
   state = {
     countdown: {
-      countdownDate: "2020-12-31 00:00:00"
+      countdownDate: "2020-12-30 00:00:00"
     },
     logo: {
       alt: "Spinning Gear",
